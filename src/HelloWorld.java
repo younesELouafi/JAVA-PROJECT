abd21b154ec74f1de74d5e8699fa4c510e1e16b0
YassirBoutrouft <boutrouft.yassir@gmail.com>
2023-01-15 21:29:26 +0100

    I added a new class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
-
+		System.out.println("Hello from Yassir Boutrouft");
 	}
 
 }
+
+
+
---
+++ src/Yassir.java
@@ -0,0 +1,10 @@
+public class Yassir {
+	
+	
+	public static int somme(int a, int b)
+	
+    {
+        return a+b;
+    }
+
+}
